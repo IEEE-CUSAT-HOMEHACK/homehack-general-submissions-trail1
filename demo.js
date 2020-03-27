af00b712161035c18a7f6fc41f71ac434364fb27
Vasant Joseph <vasantjoseph@live.com>
2020-03-27 10:09:50 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,100 @@
+//This is a Single line comment
+/* 
+This is a multi line Comment
+This is a multi line Comment
+This is a multi line Comment
+*/
+/*
+var number1 = 3;
+var number2 = 5; 
+console.log(number1, number2);
+var string1 = "Bafooon!!!";
+console.log(string1);
+var vasant= false;
+console.log(vasant);
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+number1++;
+*/
+/*
+var a,b,c,d,e;
+a=5;
+b=6;
+c=1;
+d=2;
+e=3;
+console.log(a+b);
+console.log(b-c);
+console.log(c/d);
+e++;
+console.log(e);
+e--;
+console.log(e);
+
+
+//Compond assisgment
+var a;
+a=10;
+a+=3;
+console.log(a);
+a-=6;
+console.log(a);
+
+
+//string
+
+var t1="I will study today.";
+var t2="Hope i can Finish maths."
+//console.log(t1);
+//console.log(t2);
+console.log(t1 + t2);
+console.log(t1.length);
+console.log(t1[3]);
+
+
+//Array
+var n=[1,2,3];
+var any=[1,"Shruti",3.3,true, false,null];
+console.log(any);
+
+
+var nestme= [1,2, [3,4],["Bafoon", "shruti"]];
+console.log(nestme);
+console.log(nestme[3][1]);
+
+
+var t1="What happened to you yaar!?";
+console.log(t1);
+//console.log(t1.slice(start elements position , end elememt position));
+console.log(t1.slice(21,25)); // can use -ve value to count from the rigth side
+console.log(t1.substring(21,25));
+console.log(t1.substr(21,4));// defult it will show it end.
+
+
+var veggies=['capsicum', 'carrot', 'poatato','beetroot'];
+console.log(veggies);
+veggies.shift();
+veggies.unshift('asparagus')
+veggies.pop();
+veggies.pop();
+veggies.push('broccoflower');
+veggies.push('beetroot');
+
+
+//veggies.pop('potato');
+//veggies.pop('capsicum');
+console.log(veggies);
+
+*/
+
+function B(f,g)
+{
+    console.log(f+g);
+
+   
+}    
+
+B(10,2);
